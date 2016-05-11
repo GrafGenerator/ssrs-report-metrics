2ef980604f5c8aed07f3c4b0385bd2d224d6cf45
Nikita Ivanov <grafgenerator@gmail.com>
2016-05-11 19:02:33 +0600

    added server connection class

---
+++ src/GrafGenerator.ReportMetrics.ReportingServices/Connection/ServerConnection.cs
@@ -0,0 +1,52 @@
+﻿using GrafGenerator.ReportMetrics.ReportingServices.Clients;
+
+namespace GrafGenerator.ReportMetrics.ReportingServices.Connection
+{
+	public class ServerConnection
+	{
+		public string ReportingServiceUrl { get; private set; }
+		public string ExecutionServiceUrl { get; private set; }
+
+		public bool UseDefaultCredentials { get; private set; }
+
+
+
+		public ReportingService2010 ReportingService { get; private set; }
+		public ReportExecutionService ExecutionService { get; private set; }
+
+
+
+		private ServerConnection(string server, bool useDefaultCredentials)
+		{
+			var fixedServerUrl = server.TrimEnd('/');
+			var reportSvcUrl = fixedServerUrl + "/reportservice2010.asmx";
+			var reportExecUrl = fixedServerUrl + "/reportexecution2005.asmx";
+
+			ReportingServiceUrl = reportSvcUrl;
+			ExecutionServiceUrl = reportExecUrl;
+			UseDefaultCredentials = useDefaultCredentials;
+
+			ReportingService = new ReportingService2010
+			{
+				Url = reportSvcUrl,
+				UseDefaultCredentials = useDefaultCredentials
+			};
+
+			ExecutionService = new ReportExecutionService()
+			{
+				Url = reportExecUrl,
+				UseDefaultCredentials = useDefaultCredentials
+			};
+		}
+
+		public static ServerConnection Create(string url, bool useDefaultCredentials)
+		{
+			return new ServerConnection(url, useDefaultCredentials);
+		}
+
+		public static ServerConnection Default
+		{
+			get { return Create("http://localhost/ReportServer", true); } // todo: introduce global configuration
+		}
+	}
+}
