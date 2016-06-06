7a5d3a4b08904fad35384e2e658d733c9bd44cb9
Nikita Ivanov <grafgenerator@gmail.com>
2016-06-06 17:22:13 +0600

    fixes; first tests;

--- src/GrafGenerator.ReportMetrics.Tests/ServerConnectionFixture.cs
+++ src/GrafGenerator.ReportMetrics.Tests/ServerConnectionFixture.cs
@@ -1,9 +1,29 @@
 ﻿using NUnit.Framework;
+using GrafGenerator.ReportMetrics.ReportingServices.Connection;
 
 namespace GrafGenerator.ReportMetrics.Tests
 {
 	[TestFixture]
-    public class ServerConnectionFixture
-    {
-    }
+	public class ServerConnectionFixture
+	{
+        [Test]
+		public void Test_ReportingService_ConnectionExist()
+		{
+			var connection = ServerConnection.Create("http://uk-sql01/ReportServer", true); // assume connection to localhost
+			Assert.That(connection.ReportingService, Is.Not.Null);
+
+			var extensionsList = connection.ReportingService.ListExtensions("All");
+			Assert.That(extensionsList.Length, Is.GreaterThan(0));
+		}
+
+        [Test]
+		public void Test_ReportExecutionService_ConnectionExist()
+		{
+			var connection = ServerConnection.Create("http://uk-sql01/ReportServer", true); // assume connection to localhost
+			Assert.That(connection.ReportingService, Is.Not.Null);
+
+			var extensionsList = connection.ExecutionService.ListRenderingExtensions();
+			Assert.That(extensionsList.Length, Is.GreaterThan(0));
+		}
+	}
 }
