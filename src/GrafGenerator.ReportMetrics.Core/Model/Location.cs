a7994e32e44f2af11690a094ce251cde5be1958c
Nikita Ivanov <grafgenerator@gmail.com>
2016-06-08 17:05:49 +0600

    location and location sets

---
+++ src/GrafGenerator.ReportMetrics.Core/Model/Location.cs
@@ -0,0 +1,22 @@
+﻿namespace GrafGenerator.ReportMetrics.Core.Model
+{
+	public class Location
+	{
+		public string Server { get; private set; }
+		public string Folder { get; private set; }
+		public string Name { get; private set; }
+
+		private Location(string name, string server, string folder)
+		{
+			Name = name;
+			Server = server;
+			Folder = folder;
+		}
+
+
+		public Location Create(string name, string server, string folder)
+		{
+			return new Location(name, server, folder);
+		}
+	}
+}
---
+++ src/GrafGenerator.ReportMetrics.Core/Model/LocationSet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GrafGenerator.ReportMetrics.Core.Model
+{
+	public class LocationSet
+	{
+		private readonly Dictionary<string, Location> _locations;
+
+
+		private LocationSet(Dictionary<string, Location> locations)
+		{
+			_locations = locations;
+		}
+
+		public LocationSet New(string name, Location location)
+		{
+			if (_locations.ContainsKey(name))
+				_locations[name] = location;
+			else
+				_locations.Add(name, location);
+
+			return new LocationSet(_locations);
+		}
+
+
+		public Location this[string index] => _locations[index];
+
+
+		public static LocationSet Create()
+		{
+			return new LocationSet(null);
+		}
+	}
+}
