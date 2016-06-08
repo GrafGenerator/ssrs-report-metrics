c519c194330dc1c7eab66c3f71eb271780c9e3c3
Nikita Ivanov <grafgenerator@gmail.com>
2016-06-08 15:51:01 +0600

    introduce param set

---
+++ src/GrafGenerator.ReportMetrics.Core/ParamSet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GrafGenerator.ReportMetrics.ReportingServices.ReportAccessWrappers;
+
+namespace ReportMetrics.Core
+{
+	public class ParamSet
+	{
+		public string Name { get; }
+		private readonly Dictionary<string, string> _parameters = new Dictionary<string, string>();
+
+
+		private ParamSet(string name, ParamPack parameters)
+		{
+			Name = name;
+			MergeInternal(parameters);
+		}
+
+		private void MergeInternal(ParamPack parameters)
+		{
+			var other = parameters.ToDictionary(p => p.Key, p => p.Value);
+
+			foreach (var p in other)
+			{
+				if (_parameters.ContainsKey(p.Key))
+					_parameters[p.Key] = p.Value;
+				else
+					_parameters.Add(p.Key, p.Value);
+			}
+		}
+
+
+		public ParamSet Merge(ParamPack parameters)
+		{
+			var newSet = new ParamSet(Name, ParamPack.Create(_parameters));
+			newSet.MergeInternal(parameters);
+
+			return newSet;
+		}
+
+
+		public static ParamSet Create(string name, ParamPack parameters)
+		{
+			return new ParamSet(name, parameters);
+		}
+	}
+}
--- src/GrafGenerator.ReportMetrics.ReportingServices/ReportAccessWrappers/ParamPack.cs
+++ src/GrafGenerator.ReportMetrics.ReportingServices/ReportAccessWrappers/ParamPack.cs
@@ -1,37 +1,60 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GrafGenerator.ReportMetrics.ReportingServices.ReportAccessWrappers
 {
-	public class ParamPack
+	public class ParamPack: IEnumerable<KeyValuePair<string, string>>
 	{
-		private readonly List<ParameterValue> _parameters;
+		private readonly Dictionary<string, string> _parameters;
 
 		private ParamPack()
 		{
-			_parameters = new List<ParameterValue>();
+			_parameters = new Dictionary<string, string>();
 		}
+
+		private ParamPack(Dictionary<string, string> parameters)
+		{
+			_parameters = parameters;
+		}
+
+
 
 
 		public ParamPack Add(string name, string value)
 		{
-			_parameters.Add(new ParameterValue
-			{
-				Name = name,
-				Value = value
-			});
+			if (_parameters.ContainsKey(name))
+				_parameters[name] = value;
+			else
+				_parameters.Add(name, value);
 
 			return this;
 		}
 
 		public ParameterValue[] Pack()
 		{
-			return _parameters.ToArray();
+			return _parameters.Select(kv => new ParameterValue {Name = kv.Key, Value = kv.Value}).ToArray();
 		}
 
 
 		public static ParamPack Create()
 		{
 			return new ParamPack();
+		}
+
+		public static ParamPack Create(Dictionary<string, string> parameters)
+		{
+			return new ParamPack(parameters);
+		}
+
+		public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+		{
+			return _parameters.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
 		}
 	}
 }
